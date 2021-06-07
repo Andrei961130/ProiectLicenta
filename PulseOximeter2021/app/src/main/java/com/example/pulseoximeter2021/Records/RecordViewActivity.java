897dd9ce527d23bacad6b36a29bc409064f706d7
Andrei Bucur <andrei.bucur@cst.ro>
2021-06-07 15:16:21 +0300

    [UPDATE] Finished the RecordViewActivity layout

--- PulseOximeter2021/app/src/main/java/com/example/pulseoximeter2021/Records/RecordViewActivity.java
+++ PulseOximeter2021/app/src/main/java/com/example/pulseoximeter2021/Records/RecordViewActivity.java
@@ -1,17 +1,13 @@
 package com.example.pulseoximeter2021.Records;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
 import android.widget.TextView;
 
 import com.example.pulseoximeter2021.DataLayer.Models.Firebase.Record;
 import com.example.pulseoximeter2021.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Locale;
 
@@ -26,7 +22,7 @@ public class RecordViewActivity extends AppCompatActivity {
     private TextView tvAvg;
     private TextView tvOxygen;
     private TextView tvTemp;
-    private TextView tvMsg;
+    private TextInputEditText tiMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +32,8 @@ public class RecordViewActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         record = (Record) extras.getSerializable("record");
 
-//        initialiseViews();
-//        setupRecordInformation();
+        initialiseViews();
+        setupRecordInformation();
     }
 
     private void initialiseViews() {
@@ -48,20 +44,20 @@ public class RecordViewActivity extends AppCompatActivity {
         tvAvg = findViewById(R.id.activity_record_view_tv_average);
         tvOxygen = findViewById(R.id.activity_record_view_tv_oxygen);
         tvTemp = findViewById(R.id.activity_record_view_tv_temperature);
-        tvMsg = findViewById(R.id.activity_record_view_tv_message);
+        tiMsg = findViewById(R.id.activity_record_view_ti_message);
     }
 
     private void setupRecordInformation() {
         tvDate.setText(record.getDateAndTime().split(" ")[0]);
         tvTime.setText(record.getDateAndTime().split(" ")[1]);
-        tvRange.setText(String.format(Locale.ENGLISH, "%d BPM - %d BPM", record.getMinBpmValue(), record.getMaxBpmValue()));
+        tvRange.setText(String.format(Locale.ENGLISH, "%d - %d BPM", record.getMinBpmValue(), record.getMaxBpmValue()));
         tvLength.setText(String.format(Locale.ENGLISH, "%d seconds", record.getLength()));
         tvAvg.setText(String.format(Locale.ENGLISH, "%d BPM", record.getAverageBpmValue()));
         tvOxygen.setText(String.format(Locale.ENGLISH, "%d %%", record.getOxygen()));
-        tvTemp.setText(String.format(Locale.ENGLISH, "%.1f C", record.getTemperature()));
+        tvTemp.setText(String.format(Locale.ENGLISH, "%.1f", record.getTemperature()));
 
         if(record.getMessage() != "")
-            tvMsg.setText(record.getMessage());
+            tiMsg.setText(record.getMessage());
     }
 
 
